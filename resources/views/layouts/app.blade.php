a2c5d8518a39c2afd578a6c322ddd024105ad91b
Mohammad Bashir Zadran <mohammadbashirzadran@gmail.com>
2026-02-19 16:12:15 +0430

    updateapp.php for it's active class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,21 +76,21 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="{{ route('home') }}" class="nav-item nav-link active">Home</a>
-                <a href="{{ route('about') }}" class="nav-item nav-link">About</a>
-                <a href="{{ route('service') }}" class="nav-item nav-link">Service</a>
-                <a href="{{ route('project') }}" class="nav-item nav-link">Project</a>
+                <a href="{{ route('home') }}" class="nav-item nav-link {{ request()->routeIs('home') ? 'active' : '' }}">Home</a>
+                <a href="{{ route('about')}}" class="nav-item nav-link  {{ request()->routeIs('about') ? 'active' : '' }}">About</a>
+                <a href="{{ route('service') }}" class="nav-item nav-link {{ request()->routeIs('service') ? 'active' : '' }}">Service</a>
+                <a href="{{ route('project') }}" class="nav-item nav-link {{ request()->routeIs('project') ? 'active' : '' }}">Project</a>
                 <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <a href="#" class="nav-link dropdown-toggle {{ request()->routeIs('feature', 'quote', 'team', 'testimonial') ? 'active' : '' }}" data-bs-toggle="dropdown">Pages</a>
                     <div class="dropdown-menu bg-light m-0">
-                        <a href="{{ route('feature') }}" class="dropdown-item">Feature</a>
+                        <a href="{{ route('feature') }}" class="dropdown-item ">Feature</a>
                         <a href="{{ route('quote') }}" class="dropdown-item">Free Quote</a>
                         <a href="{{ route('team') }}" class="dropdown-item">Our Team</a>
                         <a href="{{ route('testimonial') }}" class="dropdown-item">Testimonial</a>
                         <a href="{{ route('404') }}" class="dropdown-item">404 Page</a>
                     </div>
                 </div>
-                <a href="{{ route('contact') }}" class="nav-item nav-link">Contact</a>
+                <a href="{{ route('contact') }}" class="nav-item nav-link {{ request()->routeIs('contact') ? 'active' : '' }}">Contact</a>
             </div>
             <a href="" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Get A Quote<i class="fa fa-arrow-right ms-3"></i></a>
         </div>
