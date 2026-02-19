a2e907c9bb529d5410cf07a9384f1107a332079d
Mohammad Bashir Zadran <mohammadbashirzadran@gmail.com>
2026-02-19 15:26:50 +0430

    updateweb.php

--- routes/web.php
+++ routes/web.php
@@ -2,49 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+use App\Http\Controllers\Controller;
+/*****************  Page Controller  *****************/
+use App\Http\Controllers\pageController;
 
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
 
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
-
-Route::get('/service', function () {
-    return view('service');
-})->name('service');
-
-
-Route::get('/project', function () {
-    return view('project');
-})->name('project');
-
-Route::get('/feature', function () {
-    return view('feature');
-})->name('feature');
-
-
-Route::get('/quote', function () {
-    return view('quote');
-})->name('quote');
-
-Route::get('/team', function () {
-    return view('team');
-})->name('team');
-
-
-Route::get('/testimonial', function () {
-    return view('testimonial');
-})->name('testimonial');
-
-
-Route::get('/404', function () {
-    return view('404');
-})->name('404');
+Route::controller(pageController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+    Route::get('/about', 'about')->name('about');
+    Route::get('/contact', 'contact')->name('contact');
+    Route::get('/services', 'services')->name('service');
+    Route::get('/features', 'features')->name('feature');
+    Route::get('/project', 'project')->name('project');
+    Route::get('/team', 'team')->name('team');
+    Route::get('/testimonial', 'testimonial')->name('testimonial');
+    Route::get('/quote', 'quote')->name('quote');
+    Route::get('/404', 'errorhandle')->name('404');
+});
